155984739f5613567f58053813187f198d9f81df
Katy <komal.vadukul@gmail.com>
2018-06-07 21:37:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    (board[index] == " ") && index.between?(0, 8) && !position_taken?(board, index)? true : false
+    (board[index] == " ") && index.between?(0, 8) && !position_taken?(index)? true : false
   end
 
   def turn_count
