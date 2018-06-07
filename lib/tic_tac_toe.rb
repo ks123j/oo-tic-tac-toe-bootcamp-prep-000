050c75eeccb5996c03c6355142637af319daaa1a
Katy <komal.vadukul@gmail.com>
2018-06-07 21:29:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
     puts " #{board[3]} " "|" " #{board[4]} " "|" " #{board[5]} "
     puts "-----------"
     puts " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} "
-end
+  end
 
   def input_to_index(input)
     user_input = input.to_i
