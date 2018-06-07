0a7984f3b09835ae3cfe9ab585471bd5efeca614
Katy <komal.vadukul@gmail.com>
2018-06-07 22:04:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,13 +90,13 @@ class TicTacToe
   end
 
   if won?
-  our_winner = winner
-  if our_winner == "X"
-    puts "Congratulations X!"
-  else
-    puts "Congratulations O!"
+    our_winner = winner
+    if our_winner == "X"
+      puts "Congratulations X!"
+    else
+      puts "Congratulations O!"
+    end
   end
-end
 
   if draw?
     puts "Cat's Game!"
